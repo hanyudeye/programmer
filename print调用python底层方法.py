6c95183ee898e96be261d8d790c1bc76450544a7
hanyudeye <hanyudeye@163.com>
2023-12-18 16:57:11 +0800

    w

---
+++ print调用python底层方法.py
@@ -0,0 +1,4 @@
+# print 默认调用 sys.stdout.write 方法
+import sys
+
+sys.stdout.write("你好")
---
+++ 交换两者值.py
@@ -0,0 +1,16 @@
+a=5
+b=4
+
+a=a+b
+b=a-b
+a=a-b
+
+print("a=",a,"b=",b)
+
+# 或者 
+
+a=5
+b=4
+a,b=b,a
+
+print("a=",a,"b=",b)
