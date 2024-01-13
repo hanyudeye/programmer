973248bfd6e3d32346c171d4d3852d3670b54a99
hanyudeye <hanyudeye@163.com>
2024-01-13 16:52:24 +0800

    w

---
+++ 0学习/php.php
@@ -0,0 +1,20 @@
+<?php
+
+$str = "hello ,world ";
+
+// echo str_contains($str,"hello");
+$tosearch = "这行字里有xxx 鲨鱼 两个字吗Z?";
+$string = 'Awesome cheatsheets';
+// echo strpos($tosearch,'鲨鱼') ;
+
+// echo mb_strpos($tosearch,'鲨鱼');
+
+// echo strpos("I love php, I love php too!","php");
+// echo true;
+
+// echo mb_strrev($tosearch);
+// echo mb_substr($tosearch,1,3);
+// echo strrev($string);
+// echo strrev("nihao");
+
+print_r(["a"=>33,"b"=>33]);
